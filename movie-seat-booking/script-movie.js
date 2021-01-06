bd12b49195958e009389dfbe5767815c189333f8
Jamile Radloff <jamilemarcele@gmail.com>
2021-01-06 13:40:26 +0100

    JS working

--- movie-seat-booking/script-movie.js
+++ movie-seat-booking/script-movie.js
@@ -5,8 +5,32 @@ const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
 // another way to change to number is doing +movieSelect.value
-const ticketPrice = parseInt(movieSelect.value);
+let ticketPrice = +movieSelect.value;
 
+// Update total and count
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const selectedSeatsCount = selectedSeats.length;
+
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Movie select event
+movieSelect.addEventListener('change', e => {
+    ticketPrice = +e.target.value
+    updateSelectedCount();
+})
+
+// Seat click event
 container.addEventListener('click', (e) => {
-    console.log(e.target);
+    if (
+        e.target.classList.contains('seat') &&
+        !e.target.classList.contains('occupied')
+    ) {
+        e.target.classList.toggle('selected');
+
+        updateSelectedCount();
+    }
 })
