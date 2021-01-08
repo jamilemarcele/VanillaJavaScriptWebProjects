11d34958160d57944f9cf6a9000bfb97d85e20f7
Jamile Radloff <jamilemarcele@gmail.com>
2021-01-08 15:43:44 +0100

    JS forEach add user btn done

---
+++ dom-array-methods/script-dom.js
@@ -0,0 +1,51 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionaires = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+// Array with the user, name, money
+let data = [];
+
+const getRandomUser = async () => {
+    const res = await fetch('https://randomuser.me/api');
+    const data = await res.json();
+
+    const user = data.results[0];
+
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random() * 1000000)
+    };
+
+    addData(newUser);
+}
+
+// Add new obj to data arr
+const addData = obj => {
+    data.push(obj);
+
+    updateDom();
+}
+
+// Update DOM
+const updateDom = (providedData = data) => {
+//    Clear main div
+    main.innerHTML = '<h2><strong>Person</strong>Wealth</h2>';
+
+    providedData.forEach(item => {
+        const element = document.createElement('div');
+        element.classList.add('person');
+        element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+        main.appendChild(element);
+    });
+}
+
+// Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+const formatMoney = (number) => {
+    return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+// Event listener
+addUserBtn.addEventListener('click', getRandomUser);
