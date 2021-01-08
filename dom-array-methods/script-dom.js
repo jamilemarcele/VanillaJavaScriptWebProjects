f04e08a30719652411e9b27373cf77f9decea09f
Jamile Radloff <jamilemarcele@gmail.com>
2021-01-08 16:17:16 +0100

    JS reduce method done

--- dom-array-methods/script-dom.js
+++ dom-array-methods/script-dom.js
@@ -45,6 +45,15 @@ const showMillionaires = () => {
     updateDom();
 }
 
+// Calculate the total wealth
+const calculateWealth = () => {
+    const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+    const wealthEl = document.createElement('div');
+    wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+    main.appendChild(wealthEl);
+}
+
 // Add new obj to data arr
 const addData = obj => {
     data.push(obj);
@@ -75,5 +84,6 @@ addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
 sortBtn.addEventListener('click', sortByRichest);
 showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
 
 
