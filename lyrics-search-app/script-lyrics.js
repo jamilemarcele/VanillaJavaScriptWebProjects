9a6d3d6c5ffd9a9bcaef6af36b4d88f1b7a23d78
Jamile Radloff <jamilemarcele@gmail.com>
2021-01-20 10:54:34 +0100

    JS lyrics search in progress

---
+++ lyrics-search-app/script-lyrics.js
@@ -0,0 +1,34 @@
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const result = document.getElementById("result");
+// More is for the pagination results
+const more = document.getElementById("more");
+
+const apiURL = "https://api.lyrics.ovh";
+
+// Search by song or artist
+const searchSongs = async (term) => {
+    const res = await fetch(`${apiURL}/suggest/${term}`);
+    const data = await res.json();
+
+    showData(data);
+}
+
+// Show song and artist in DOM
+const showData = (data) => {
+
+}
+
+
+// Event listeners
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value.trim();
+
+    if (!searchTerm) {
+        alert("Please type in a search term");
+    } else {
+        searchSongs(searchTerm);
+    }
+});
